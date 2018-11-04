e66ffe503ab0a6140f244ea89e142f8918c1a479
Anatoly <AnatoliiAndreev19@gmail.com>
2018-11-04 23:00:59 +0300

    Аудио на сайте

--- docs/js/audio.js
+++ docs/js/audio.js
@@ -1,5 +1,39 @@
 $(document).ready(function(){
 
+var audio = $('#audioTrack')[0];
+// console.log(audio);
+
+$playBtn = $('#audio-play');
+$pauseBtn = $('#audio-pause');
+// селектор по атрибутоам - используем квадратные скобки
+$audioControl = $('[data-type="audio-control"]');
+
+setTimeout(initAudioPlayer, 800);
+
+function initAudioPlayer(){
+	$playBtn.parent().show();
+	audio.loop = true;
+	audio.play();
+	
+	$audioControl.click(function(){
+
+		if ( audio.paused){
+
+			audio.play();
+			$playBtn.parent().hide();
+			$pauseBtn.parent().show();
+
+		} else {
+			audio.pause();
+			$playBtn.parent().show();
+			$pauseBtn.parent().hide();
+
+		}
+
+	}); 
+}
+
+// audio.play();
 
 
 });
